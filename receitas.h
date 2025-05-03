cac0660af88665c248b736317eafe3f5cccdc83e
Coelho09 <rrapozao7@live.com>
2025-05-03 15:54:27 -0300

    receitas.h att

--- receitas.h
+++ receitas.h
@@ -1,5 +1,7 @@
+
 #ifndef RECEITAS_H
 #define RECEITAS_H
+#include "dados.h"
 #include "ingredientes.h"
 
 typedef struct noR{
@@ -8,17 +10,18 @@ typedef struct noR{
     struct noR* prox;
 } NoR;
 
-
 typedef struct ListaR{
     NoR* inicio;
     NoR* fim;
     int quantidade;
 } ListaR;
 
+
 ListaR* criarListaR();
 NoR* criarNovoNoR(char receita[]);
 void inserirR(ListaR* r, char receita[]);
 void removerR(ListaR* r, char receita[]);
-
+NoR* procurarR(ListaR* r, char receita[]);
+NoR* percorrerR(ListaR* r);
 
 #endif

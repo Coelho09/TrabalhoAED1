e8adaba389efb40421af7955af5a785d04c45f1a
Coelho09 <rrapozao7@live.com>
2025-05-03 12:42:36 -0300

    Update main Trabalho.c

--- main Trabalho.c
+++ main Trabalho.c
@@ -1,11 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "ingredientes.h"
+#include "receitas.h"
 
 
 int main()
 {
     ListaI* l = criarListaI();
+    ListaR* R = criarListaR();
+    inserirR(R, "Strogonoff");
+    inserirI(l, "Cominho");
+    percorrerI(l);
 
     return 0;
 }

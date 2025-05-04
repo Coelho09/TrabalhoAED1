f1e388b997077906058339b9425403b928e9f343
Samuel Amorim <167945818+samuel-famorim@users.noreply.github.com>
2025-05-03 23:38:27 -0300

    Update dados.h

--- dados.h
+++ dados.h
@@ -1,6 +1,10 @@
 #ifndef DADOS_H
 #define DADOS_H
 
+/*
+   Declarações antecipadas (forward declarations) das structs ListaR e ListaI.
+   Isso permite que outros arquivos de cabeçalho as utilizem sem causar dependência circular.
+*/
 typedef struct ListaR ListaR;
 typedef struct ListaI ListaI;
 

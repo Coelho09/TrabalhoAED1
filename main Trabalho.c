dc68b239914b05da7cb6f722753cdb64485c3ff4
Coelho09 <rrapozao7@live.com>
2025-05-05 22:40:16 -0300

    Update main Trabalho.c

--- main Trabalho.c
+++ main Trabalho.c
@@ -22,6 +22,7 @@ int main() {
     int antes;
     char receita[100];
     char ingrediente[100];
+    char Ningrediente[100];
 
      /*
       Loop principal do menu. O programa só sai quando a opção 0 for escolhida.
@@ -35,9 +36,11 @@ int main() {
         printf("3 - Remover Receita\n");
         printf("4 - Remover Ingrediente de Receita\n");
         printf("5 - Ver todas as receitas\n");
-        printf("6 - Marcar um Ingrediente como Essencial\n");
-        printf("7 - Marcar alguma Receita como Favorita\n");
-        printf("8 - Ver todos os Ingredientes Essenciais e Receitas favoritas\n");
+        printf("6 - Buscar Ingrediente\n");
+        printf("7 - Substituir Ingrediente\n");
+        printf("8 - Marcar um Ingrediente como Essencial\n");
+        printf("9 - Marcar alguma Receita como Favorita\n");
+        printf("10 - Ver todos os Ingredientes Essenciais e Receitas favoritas\n");
         printf("0 - Sair\n");
         printf("Escolha uma opcao: ");
         scanf("%d", &opcao);
@@ -82,9 +85,10 @@ int main() {
                     printf("Nome do ingrediente: ");
                     fgets(ingrediente, sizeof(ingrediente), stdin);
                     ingrediente[strcspn(ingrediente, "\n")] = '\0';
-            
-                    inserirI(R, receita, ingrediente);
-            
+                    
+                    antes = inserirI(R, receita, ingrediente);
+                    
+                    if(antes == 1){
                     char resposta;
                     printf("Deseja inserir mais um ingrediente? (s/n): ");
                     scanf(" %c", &resposta);
@@ -92,8 +96,8 @@ int main() {
             
                     if (resposta != 's' && resposta != 'S') {
                         break;
-                    }
-            
+                        }
+                    } else break;
                 } while (1);
                 break;
 
@@ -135,7 +139,40 @@ int main() {
 
             case 5:
                 percorrerR(R);
-                system("pause");
+                break;
+            
+                /*
+                  Opção para procurar as receitas que possuem o ingrediente digitado.
+                  Imprime o nome das receitas que possuirem tal ingrediente.
+                */
+                
+            case 6:
+                printf("Digite o nome do ingrediente que você esta buscando: ");
+                fgets(ingrediente, sizeof(ingrediente), stdin);
+                ingrediente[strcspn(ingrediente, "\n")] = '\0';
+                
+                buscarIngrediente(R, ingrediente);
+                break;
+                
+                /*
+                  Opção para substituir algum ingrediente.
+                  Procura o ingrediente na receita digitada e substitui pelo novo ingrediente.
+                */
+                
+            case 7:
+                printf("Digite o nome da receita em que esse ingrediente se encontra: ");
+                fgets(receita, sizeof(receita), stdin);
+                receita[strcspn(receita, "\n")] = '\0';
+                
+                printf("Digite o nome do ingrediente que você deseja substituir: ");
+                fgets(ingrediente, sizeof(ingrediente), stdin);
+                ingrediente[strcspn(ingrediente, "\n")] = '\0';
+                
+                printf("Digite o nome do novo ingrediente: ");
+                fgets(Ningrediente, sizeof(Ningrediente), stdin);
+                Ningrediente[strcspn(Ningrediente, "\n")] = '\0';
+                
+                substituir(R, receita, ingrediente, Ningrediente);
                 break;
             
                 /*
@@ -143,7 +180,7 @@ int main() {
                   a função essencial() procura a receita digitada pelo usuario e altera o campo ensencial para 1(verdadeiro).
                 */
             
-            case 6:
+            case 8:
                 printf("Digite o nome da receita em que esse ingrediente se encontra: ");
                 fgets(receita, sizeof(receita), stdin);
                 receita[strcspn(receita, "\n")] = '\0';
@@ -155,7 +192,7 @@ int main() {
                   a função favorita() procura a receita digitada pelo usuario e altera o campo favorita para 1(verdadeiro).
                 */
                 
-            case 7:
+            case 9:
                 printf("Digite o nome da receita que você deseja favoritar: ");
                 fgets(receita, sizeof(receita), stdin);
                 receita[strcspn(receita, "\n")] = '\0';
@@ -166,7 +203,7 @@ int main() {
                   Impressão de todas as receitas favoritas e os ingredientes essenciais da lista de receitas.
                 */
                 
-            case 8:
+            case 10:
                 mostraEspeciais(R);
                 break;
 
